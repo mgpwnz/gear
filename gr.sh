673b310678a020c420c9296d4908234be647a9c8
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-01-26 14:09:16 +0200

    Update gr.sh

--- gr.sh
+++ gr.sh
@@ -61,7 +61,6 @@ function firewall {
 sudo ufw allow 22:65535/tcp
 sudo ufw allow 22:65535/udp
 sudo ufw deny out from any to 10.0.0.0/8
-sudo ufw deny out from any to 192.168.0.0/16
 sudo ufw deny out from any to 100.64.0.0/10
 sudo ufw deny out from any to 198.18.0.0/15
 sudo ufw deny out from any to 169.254.0.0/16
